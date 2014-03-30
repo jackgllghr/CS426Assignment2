3dd5e6f9f119980cf6c889895d0a41da1567ee06
jackgllghr <jackgllghr@gmail.com>
2014-03-30 20:21:42 +0100

    Initial commit

---
+++ Assignment2/Assignment2/Game1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace Assignment2
+{
+	/// <summary>
+	/// This is the main type for your game
+	/// </summary>
+	public class Game1 : Microsoft.Xna.Framework.Game
+	{
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+		private Texture2D playerTexture, balls;
+		private float _angle;
+		private Vector2 playerPosition;
+		int playerHeight;
+		int playerWidth;
+		private Vector2 playerDirection;
+		private Vector2 ballPosition;
+
+		private Random _rand = new Random();
+		private int _frame = 0;
+		private int _iter = 0;
+		private int ballNo = 0;
+		int ballSize = 60;
+		int state = 0;
+		SpriteEffects facing;
+		int movementSpeed = 40;
+
+		List<Sprite> sprites;
+
+		public Game1()
+		{
+			graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+		}
+
+		/// <summary>
+		/// Allows the game to perform any initialization it needs to before starting to run.
+		/// This is where it can query for any required services and load any non-graphic
+		/// related content.  Calling base.Initialize will enumerate through any components
+		/// and initialize them as well.
+		/// </summary>
+		protected override void Initialize()
+		{
+			// TODO: Add your initialization logic here
+			_angle = 0.0f;
+			playerPosition = new Vector2(68, 50);
+			playerDirection = new Vector2(3, 3);
+			playerHeight = 200;
+			playerWidth = 200;
+
+			ballPosition.X = _rand.Next(GraphicsDevice.Viewport.Width);
+			ballPosition.Y = _rand.Next(GraphicsDevice.Viewport.Height);
+
+			sprites=new List<Sprite>();
+
+			base.Initialize();
+		}
+
+		/// <summary>
+		/// LoadContent will be called once per game and is the place to load
+		/// all of your content.
+		/// </summary>
+		protected override void LoadContent()
+		{
+			// Create a new SpriteBatch, which can be used to draw textures.
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+
+			sprites.Add(new Player(spriteBatch, Content));
+			// TODO: use this.Content to load your game content here
+		}
+
+		/// <summary>
+		/// UnloadContent will be called once per game and is the place to unload
+		/// all content.
+		/// </summary>
+		protected override void UnloadContent()
+		{
+			// TODO: Unload any non ContentManager content here
+		}
+
+		/// <summary>
+		/// Allows the game to run logic such as updating the world,
+		/// checking for collisions, gathering input, and playing audio.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Update(GameTime gameTime)
+		{
+			KeyboardState keys = Keyboard.GetState(); // Q to Quit 
+			if (keys.IsKeyDown(Keys.Q)) this.Exit();
+
+			// #4 Advance each of the sprites 
+			foreach (Sprite s in sprites)
+			{
+				s.advance();
+			}
+
+			base.Update(gameTime);
+		}
+
+		/// <summary>
+		/// This is called when the game should draw itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.Black);
+
+			// #5 Draw each sprite 
+			spriteBatch.Begin();
+			foreach (Sprite s in sprites)
+			{
+				s.draw();
+			}
+			spriteBatch.End();
+
+			base.Draw(gameTime); 
+
+		}
+		private Boolean CheckForCollision()
+		{
+			if (playerPosition.X > ballPosition.X && playerPosition.X<ballPosition.X+ballSize && playerPosition.Y >ballPosition.Y && playerPosition.Y<ballPosition.Y+ballSize)
+				return true;
+
+			else return false;
+		}
+	}
+}
---
+++ Assignment2/Assignment2/Sprite.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.GamerServices;
+
+namespace Assignment2
+{
+	public abstract class Sprite
+	{
+		public SpriteBatch spriteBatch;
+		public Texture2D texture;
+
+		public int X, Y, dirX, dirY, Width, Height;
+		public int movementSpeed=5;
+		public Vector2 dir;
+
+		public Sprite(SpriteBatch s, ContentManager c) {
+			spriteBatch = s;
+
+
+		}
+		public virtual void draw(){
+			spriteBatch.Draw(texture, new Rectangle(X, Y, Width, Height), Color.White);
+		}
+		public abstract void advance();
+	}
+	public class Player : Sprite
+	{
+		public int frame=0;
+		public int frameCount;
+		public int iterator;
+
+		public Player(SpriteBatch inBatch, ContentManager inContent)
+			: base(inBatch, inContent)
+		{
+			texture = inContent.Load<Texture2D>("Alucard");
+			X = 100;
+			Y = 100;
+			Width = 56; 
+			Height=128;
+			frameCount = 3;
+			iterator = 0;
+		}
+		public override void draw() {
+			if (iterator > 5)
+			{
+				frame++;
+				iterator = 0;
+			}
+			if (frame > frameCount) frame = 0;
+
+			iterator++;
+			spriteBatch.Draw(texture, new Rectangle(X, Y, Width, Height), new Rectangle(frame * 28, 0, 28, 64), Color.White);
+
+
+		}
+		public override void advance()
+		{
+			KeyboardState keys = Keyboard.GetState(); 
+			GamePadState pad=GamePad.GetState(PlayerIndex.One);
+			if ((Y+Height) < spriteBatch.GraphicsDevice.Viewport.Height && keys.IsKeyDown(Keys.Down) || pad.IsButtonDown(Buttons.DPadDown)) {
+				Y = Y + movementSpeed; 
+			}
+			if (Y > 0 && keys.IsKeyDown(Keys.Up) || pad.IsButtonDown(Buttons.DPadUp))  {
+				Y = Y - movementSpeed; 
+			}
+			if (X > 0 && keys.IsKeyDown(Keys.Left) || pad.IsButtonDown(Buttons.DPadLeft)) { 
+				X = X - movementSpeed; 
+			}
+			if ((X+Width)<spriteBatch.GraphicsDevice.Viewport.Width && keys.IsKeyDown(Keys.Right) || pad.IsButtonDown(Buttons.DPadRight)){
+				X = X + movementSpeed;
+			}
+		}
+	}
+}
